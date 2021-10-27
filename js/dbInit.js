e5ae94480301568130957da341408409efd4911a
BrianMCiszewskiA <brianmciszewski@hotmail.com.ar>
2021-10-27 20:42:04 -0300

    testing firebase pt3
    Volvio el error del "addAssitant() is not defined"

--- js/dbInit.js
+++ js/dbInit.js
@@ -41,3 +41,30 @@ function addAssistant(){
     console.error("Error adding document: ", e);
     }
 }
+//contenido del forms
+var forms = document.querySelectorAll('#newAssistant')
+
+console.dir(forms);
+
+forms[0].addEventListener('submit', function (event) {
+    resetFields();
+    event.preventDefault()
+    event.stopPropagation()
+
+    checkFields();
+    if(allFieldsAreValid()){
+        addAssistant();
+    }
+})
+
+function checkFields(){
+
+}
+
+function allFieldsAreValid(){
+    //testing purposes
+    return true;
+}
+function resetFields(){
+    
+}
