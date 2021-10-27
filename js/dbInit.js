839f17c7788b7b188358c86a6c9d4ac4c91fdefe
BrianMCiszewskiA <brianmciszewski@hotmail.com.ar>
2021-10-27 20:38:05 -0300

    Testing firebase pt2

--- js/dbInit.js
+++ js/dbInit.js
@@ -19,3 +19,25 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+/**const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");*/
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+
+function addAssistant(){
+    try {
+    const docRef = addDoc(collection(db, "asistentes"), {
+        nombre: document.getElementById("name").value,
+        apellido: document.getElementById("surname").value,
+        mail: document.getElementById("mail").value,
+        pais: document.getElementById("countries").value,
+        tel: document.getElementById("phone").value,
+        pTrabajo: document.getElementById("job").value
+    });
+    console.log("Document written with ID: ", docRef.id);
+    //pop up "assistant confirmed"
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
+}
