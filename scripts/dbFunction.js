e97f228f5c32195daf53f761ae4e376d80dde0fb
BrianMCiszewskiA <brianmciszewski@hotmail.com.ar>
2021-10-28 13:49:42 -0300

    firebase working
    Siento q esta atado con alambre, pero finalmente, conecta bien

--- scripts/dbFunction.js
+++ scripts/dbFunction.js
@@ -6,21 +6,7 @@ console.log("import");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCau3jxdrE1aoz0p_s2XDYle6TbPG9qlYU",
-  authDomain: "eventassist-42b50.firebaseapp.com",
-  projectId: "eventassist-42b50",
-  storageBucket: "eventassist-42b50.appspot.com",
-  messagingSenderId: "86546452983",
-  appId: "1:86546452983:web:cf748a57585a1a2b075ef2",
-  measurementId: "G-M45SGQVXGT"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-console.log(db);
 
 
 function addAssistant(){
--- scripts/dbInit.js
+++ scripts/dbInit.js
@@ -3,3 +3,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
 console.log(initializeApp);
+function test(){
+  console.log("works");
+}
+const firebaseConfig = {
+  apiKey: "AIzaSyCau3jxdrE1aoz0p_s2XDYle6TbPG9qlYU",
+  authDomain: "eventassist-42b50.firebaseapp.com",
+  projectId: "eventassist-42b50",
+  storageBucket: "eventassist-42b50.appspot.com",
+  messagingSenderId: "86546452983",
+  appId: "1:86546452983:web:cf748a57585a1a2b075ef2",
+  measurementId: "G-M45SGQVXGT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log(db);
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
